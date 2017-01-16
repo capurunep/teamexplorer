3f02afe64e70a6946bb1ae7b9749ec96f23f9b09
Purushottam Dangol <puru@Purushottams-MacBook-Pro.local>
2017-01-15 22:43:16 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Team 4';
+  project='Task to do';
+  showbuttons=false;
+
+items: FirebaseListObservable<any[]>
+
+constructor(private af: AngularFire)
+{
+this.items = af.database.list('/items');
+
+}
+over()
+{
+  this.showbuttons=true;
+
+}
+offmouse()
+{
+  this.showbuttons=false;
+}
 }
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    
+    apiKey: "AIzaSyCcelEwGYbQNTswIc6sC0sytQ2ebh-NKFU",
+    authDomain: "teamexplorer-25f86.firebaseapp.com",
+    databaseURL: "https://teamexplorer-25f86.firebaseio.com",
+    storageBucket: "teamexplorer-25f86.appspot.com"
+
+}
